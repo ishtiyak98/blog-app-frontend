9bb1f01c013ca24d461cfa852e8cf34a3b93a409
ishtiyak98 <rifatishtiyak@gmail.com>
2022-11-07 21:21:28 +0600

    title & readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Blog App
+
+<li>Homepage  - At least ten posts</li>
+<li>Post page - Post details</li>
+<li>About us - About yourself</li>
--- src/components/PostCard/PostCard.js
+++ src/components/PostCard/PostCard.js
@@ -16,55 +16,56 @@ const PostCard = ({ post }) => {
   }, []);
 
   return (
-    <div
-      data-aos="zoom-in"
-      className="space-y-4 cursor-pointer hover:scale-105 transition-transform ease-in-out duration-300 hover:shadow-xl p-6"
-      onClick={() => {
-        navigate(`/posts/${post.id}`);
-      }}
-    >
-      <div className="flex items-center space-x-4 text-gray-600 hover:text-blue-700 hover:cursor-pointer">
-        <img
-          className="w-[35px] h-[35px] rounded-full ring-blue-600"
-          src={post.owner.picture}
-          alt=""
-        />
-        <div className="font-medium space-x-1 ">
-          <span>{post.owner.firstName}</span>
-          <span>{post.owner.lastName}</span>
+    <div data-aos="zoom-out">
+      <div
+        className="space-y-4 cursor-pointer hover:scale-105 transition-transform ease-in-out duration-300 hover:shadow-xl p-6"
+        onClick={() => {
+          navigate(`/posts/${post.id}`);
+        }}
+      >
+        <div className="flex items-center space-x-4 text-gray-600 hover:text-blue-700 hover:cursor-pointer">
+          <img
+            className="w-[35px] h-[35px] rounded-full ring-blue-600"
+            src={post.owner.picture}
+            alt=""
+          />
+          <div className="font-medium space-x-1 ">
+            <span>{post.owner.firstName}</span>
+            <span>{post.owner.lastName}</span>
+          </div>
         </div>
-      </div>
-      <div>
-        <img
-          className="rounded-md w-full object-cover h-[200px]"
-          src={post.image}
-          alt=""
-        />
-      </div>
-      <div className="space-y-3">
-        <h2 className="font-bold text-xl text-gray-800 normal-case">
-          {post.text}
-        </h2>
-        <div className="flex justify-between text-gray-500">
-          <div className="flex items-center space-x-4">
-            <div className="flex items-center space-x-1">
-              <AiOutlineLike
+        <div>
+          <img
+            className="rounded-md w-full object-cover h-[200px]"
+            src={post.image}
+            alt=""
+          />
+        </div>
+        <div className="space-y-3">
+          <h2 className="font-bold text-xl text-gray-800 normal-case">
+            {post.text}
+          </h2>
+          <div className="flex justify-between text-gray-500">
+            <div className="flex items-center space-x-4">
+              <div className="flex items-center space-x-1">
+                <AiOutlineLike
+                  size={"1.2em"}
+                  className="hover:cursor-pointer hover:text-blue-700"
+                ></AiOutlineLike>{" "}
+                <span className="text-sm font-medium">{post.likes}</span>
+              </div>
+              <AiOutlineComment
                 size={"1.2em"}
                 className="hover:cursor-pointer hover:text-blue-700"
-              ></AiOutlineLike>{" "}
-              <span className="text-sm font-medium">{post.likes}</span>
+              ></AiOutlineComment>
+              <AiOutlineShareAlt
+                size={"1.2em"}
+                className="hover:cursor-pointer hover:text-blue-700"
+              ></AiOutlineShareAlt>
             </div>
-            <AiOutlineComment
-              size={"1.2em"}
-              className="hover:cursor-pointer hover:text-blue-700"
-            ></AiOutlineComment>
-            <AiOutlineShareAlt
-              size={"1.2em"}
-              className="hover:cursor-pointer hover:text-blue-700"
-            ></AiOutlineShareAlt>
-          </div>
-          <div className="text-sm font-medium">
-            {post.publishDate.slice(0, 10)}
+            <div className="text-sm font-medium">
+              {post.publishDate.slice(0, 10)}
+            </div>
           </div>
         </div>
       </div>
