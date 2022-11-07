94400d2fabf4169e9a99bfb95c31177b75e392db
ishtiyak98 <rifatishtiyak@gmail.com>
2022-11-07 21:05:23 +0600

    assignment done

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,27 +4,36 @@ import PostCard from "../../components/PostCard/PostCard";
 
 const Home = () => {
   const [posts, setPosts] = useState({});
+  const [itemNumber, setItemNumber] = useState(10);
   useEffect(() => {
-    fetch("https://dummyapi.io/data/v1/post?limit=10", {
+    fetch(`https://dummyapi.io/data/v1/post?limit=${itemNumber}`, {
       headers: {
-        "app-id": "63689aa65eba72c2683e54a3",
+        "app-id": process.env.REACT_APP_apiKey,
       },
     })
       .then((res) => res.json())
       .then((data) => {
         setPosts(data);
       });
-  }, []);
-  console.log(posts);
+  }, [itemNumber]);
+
+  console.log(itemNumber);
   return (
     <div>
       <Navbar></Navbar>
-      {/* <PostCard></PostCard> */}
       <div className="px-6 py-5 lg:px-16 lg:py-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
         {posts?.data?.map((post) => (
           <PostCard key={post.id} post={post}></PostCard>
         ))}
       </div>
+      {posts && (
+        <div
+          className="bg-blue-500 w-fit mx-auto px-4 py-2 rounded text-white mb-10 hover:bg-blue-800 hover:cursor-pointer"
+          onClick={() => setItemNumber(itemNumber + 10)}
+        >
+          Show More Post
+        </div>
+      )}
     </div>
   );
 };
